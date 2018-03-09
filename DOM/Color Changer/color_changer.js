f0b9b15782907fcff89a455c65ae644241d457a5
Unknown <shadeeperez@gmail.com>
2018-03-09 10:27:07 -0700

    color changer

---
+++ DOM/Color Changer/color_changer.js
@@ -0,0 +1,9 @@
+// Variables//
+// This grabs the id of the first modal // 
+var modal = document.getElementById("modal"); 
+// This grabs the button with the id "change"
+// This allows us to call a function with a click later
+var modal = 
+    document.getElementById("modal"); 
+// This grabs the preview 
+var preview = document.getElementById("preview")
